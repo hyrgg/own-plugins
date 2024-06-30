57073180a04af13674c7387f08a1aac6e611575b
hyrgg <83886700+hyrgg@users.noreply.github.com>
2024-07-01 00:01:44 +0800

    Update stashOpenMediaPlayer.py

--- plugins/stashOpenMediaPlayer/stashOpenMediaPlayer.py
+++ plugins/stashOpenMediaPlayer/stashOpenMediaPlayer.py
@@ -2,6 +2,7 @@ import json
 import log
 import sys
 import subprocess
+from urllib.parse import unquote
 
 json_input = json.loads(sys.stdin.read())
 name = json_input['args']['name']
@@ -9,6 +10,9 @@ name = json_input['args']['name']
 if name == 'mediaplayer':
     mediaplayer_path = json_input['args']['mediaPlayerPath']
     path = json_input['args']['path']
+    path = decoded_uri = unquote(path)
+    if path.startswith('file:///'):
+        path = path.lstrip('file:///')
     log.debug(f"mediaplayer_path: {mediaplayer_path}")
     log.debug(f"{name}: {path}")
     subprocess.Popen([mediaplayer_path, path])
