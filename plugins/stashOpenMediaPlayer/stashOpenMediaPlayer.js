f9777032371d5f14ce7ba05d893d2bf750b86dbd
hyrgg <83886700+hyrgg@users.noreply.github.com>
2024-07-01 00:01:28 +0800

    Update stashOpenMediaPlayer.js

--- plugins/stashOpenMediaPlayer/stashOpenMediaPlayer.js
+++ plugins/stashOpenMediaPlayer/stashOpenMediaPlayer.js
@@ -12,23 +12,26 @@
 
     async function openMediaPlayerTask(path) {
         // fixes decodeURI breaking on %'s because they are not encoded
-        const encodedPctPath = path.replace(/%([^\d].)/, "%25$1");
+        // const encodedPctPath = path.replace(/%([^\d].)/, "%25$1");
         // decode encoded path but then encode % and # otherwise VLC breaks
-        const encodedPath = decodeURI(encodedPctPath).replaceAll('%', '%25').replaceAll('#', '%23');
+        // const encodedPath = decodeURI(encodedPctPath).replaceAll('%', '%25').replaceAll('#', '%23');
         const settings = await stash.getPluginConfig('stashOpenMediaPlayer');
-        stash.runPluginTask("stashOpenMediaPlayer", "Open in Media Player", [{"key":"path", "value":{"str": encodedPath}}, {"key":"mediaPlayerPath", "value":{"str": settings?.mediaPlayerPath}}]);
+        stash.runPluginTask("stashOpenMediaPlayer", "Open in Media Player", [{ "key": "path", "value": { "str": path } }, { "key": "mediaPlayerPath", "value": { "str": settings?.mediaPlayerPath } }]);
     }
     stash.openMediaPlayerTask = openMediaPlayerTask;
 
     // scene filepath open with Media Player
     stash.addEventListener('page:scene', function () {
         waitForElementClass('scene-file-info', function () {
-            const a = getElementByXpath("//dt[text()='Path']/following-sibling::dd/a");
-            if (a && !a.classList.contains('open-media-player')) {
-                a.classList.add('open-media-player');
-                a.addEventListener('click', async function () {
+            const translate_path = ['Path', '路径'];
+            var a;
+            for (var i = 0; i < translate_path.length; i++) {
+                var x_path = "//dt[text()='" + translate_path[i] + "']/following-sibling::dd/a";
+                a = getElementByXpath(x_path);
+                if (a && !a.classList.contains('open-media-player')) {
                     openMediaPlayerTask(a.href);
-                });
+                }
+                break;
             }
         });
     });
